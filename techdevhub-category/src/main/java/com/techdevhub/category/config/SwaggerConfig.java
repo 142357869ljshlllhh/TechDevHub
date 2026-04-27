e339507b38f48846bb6e3fad1066b2761d6618c9
142357869ljshlllhh <2893669556@qq.com>
2026-04-27 13:51:30 +0800

    @demons@20260427002@为全部服务新增Swagger接口文档配置

---
+++ techdevhub-category/src/main/java/com/techdevhub/category/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.techdevhub.category.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI categoryOpenApi() {
+        String schemeName = "BearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TechDevHub Category API")
+                        .version("v1.0.0")
+                        .description("Category module APIs"))
+                .components(new Components().addSecuritySchemes(
+                        schemeName,
+                        new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName));
+    }
+}
---
+++ techdevhub-like/src/main/java/com/techdevhub/like/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.techdevhub.like.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI likeOpenApi() {
+        String schemeName = "BearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TechDevHub Like API")
+                        .version("v1.0.0")
+                        .description("Like module APIs"))
+                .components(new Components().addSecuritySchemes(
+                        schemeName,
+                        new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName));
+    }
+}
